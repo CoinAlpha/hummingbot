09868b740c22f9da9fef3716096e3367edf2fe98
Petio Petrov <petioptrv@icloud.com>
2023-01-23 20:32:51 +0700

    (refactor) Updates batch_order_update.py

--- scripts/batch_order_update.py
+++ scripts/batch_order_update.py
@@ -1,4 +1,4 @@
-import time
+from collections import defaultdict
 from decimal import Decimal
 from typing import List
 
@@ -7,10 +7,10 @@ from hummingbot.core.data_type.common import OrderType, TradeType
 from hummingbot.core.data_type.order import Order
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
-CONNECTOR = "injective_injective_mainnet"
-TRADING_PAIR = combine_to_hb_trading_pair(base="ATOM", quote="USDT")
-AMOUNT = Decimal("0.15")
-ORDERS_INTERVAL = 20
+CONNECTOR = "okx"
+TRADING_PAIR = combine_to_hb_trading_pair(base="BTC", quote="USDT")
+AMOUNT = Decimal("0.0001")
+ORDERS_INTERVAL = 10
 PRICE_OFFSET_RATIO = Decimal("0.1")  # 10%
 
 
@@ -18,14 +18,20 @@ class BatchOrderUpdate(ScriptStrategyBase):
     markets = {CONNECTOR: {TRADING_PAIR}}
     pingpong = 0
 
-    orders_placed_ts = time.time() - ORDERS_INTERVAL
+    script_phase = 0
 
     def on_tick(self):
-        if time.time() - self.orders_placed_ts > ORDERS_INTERVAL:
-            self.refresh_orders()
-            self.orders_placed_ts = time.time()
+        if self.script_phase == 0:
+            self.place_two_orders_successfully()
+        elif self.script_phase == ORDERS_INTERVAL:
+            self.cancel_orders()
+        elif self.script_phase == ORDERS_INTERVAL * 2:
+            self.place_two_orders_with_one_zero_amount_that_will_fail()
+        elif self.script_phase == ORDERS_INTERVAL * 3:
+            self.cancel_orders()
+        self.script_phase += 1
 
-    def refresh_orders(self):
+    def place_two_orders_successfully(self):
         price = self.connectors[CONNECTOR].get_price(trading_pair=TRADING_PAIR, is_buy=True)
         orders_to_create = [
             Order(
@@ -43,23 +49,58 @@ class BatchOrderUpdate(ScriptStrategyBase):
                 price=price * (1 + PRICE_OFFSET_RATIO),
             )
         ]
-        orders_to_cancel = [
-            Order(
-                trading_pair=order.trading_pair,
-                order_type=OrderType.LIMIT,
-                trade_type=TradeType.BUY if order.is_buy else TradeType.SELL,
-                amount=order.quantity,
+
+        market_pair = self._market_trading_pair_tuple(connector_name=CONNECTOR, trading_pair=TRADING_PAIR)
+        market = market_pair.market
+
+        submitted_orders: List[Order] = market.batch_order_create(
+            orders_to_create=orders_to_create,
+        )
+
+        for order in submitted_orders:
+            self.start_tracking_limit_order(
+                market_pair=market_pair,
+                order_id=order.client_order_id,
+                is_buy=order.trade_type == TradeType.BUY,
                 price=order.price,
-                client_order_id=order.client_order_id,
-            ) for order in self.get_active_orders(connector_name=CONNECTOR)
+                quantity=order.amount,
+            )
+
+    def cancel_orders(self):
+        exchanges_to_orders = defaultdict(lambda: [])
+        exchanges_dict = {}
+
+        for exchange, order in self.order_tracker.active_limit_orders:
+            exchanges_to_orders[exchange.name].append(order)
+            exchanges_dict[exchange.name] = exchange
+
+        for exchange_name, orders_to_cancel in exchanges_to_orders.items():
+            exchanges_dict[exchange_name].batch_order_cancel(orders_to_cancel=orders_to_cancel)
+
+    def place_two_orders_with_one_zero_amount_that_will_fail(self):
+        price = self.connectors[CONNECTOR].get_price(trading_pair=TRADING_PAIR, is_buy=True)
+        orders_to_create = [
+            Order(
+                trading_pair=TRADING_PAIR,
+                order_type=OrderType.LIMIT,
+                trade_type=TradeType.BUY,
+                amount=AMOUNT,
+                price=price * (1 - PRICE_OFFSET_RATIO),
+            ),
+            Order(
+                trading_pair=TRADING_PAIR,
+                order_type=OrderType.LIMIT,
+                trade_type=TradeType.SELL,
+                amount=Decimal("0"),
+                price=price * (1 + PRICE_OFFSET_RATIO),
+            )
         ]
 
         market_pair = self._market_trading_pair_tuple(connector_name=CONNECTOR, trading_pair=TRADING_PAIR)
         market = market_pair.market
 
-        submitted_orders: List[Order] = market.batch_order_update(
+        submitted_orders: List[Order] = market.batch_order_create(
             orders_to_create=orders_to_create,
-            orders_to_cancel=orders_to_cancel,
         )
 
         for order in submitted_orders:
